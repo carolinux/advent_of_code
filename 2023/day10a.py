452a384ff5933856d8af73fab972782ecfaf1ed3
carolinux <carolinegr@gmail.com>
2023-12-11 10:47:46 +0100

    fix

--- 2023/day10a.py
+++ 2023/day10a.py
@@ -1,41 +1,64 @@
 
-s = 0
+mat = []
+S = 'S'
 
-def process(a):
-    lasts = [a[-1]]
-    while True:
-        b = []
-        for i in range(1, len(a)):
-            b.append(a[i]-a[i-1])
+with open("day10.txt", 'r') as f:
 
 
-        if all([x == 0 for x in b]):
-
-            break
-
-        lasts.append(b[-1])
-
-        a = b
-
-    curr = 0
-    for j in range(len(lasts)-1, -1, -1):
-        curr = curr + lasts[j]
+    for i,line in enumerate(f.readlines()):
+        mat.append(list(line.strip()))
+        if S in mat[-1]:
+            start = (i, mat[-1].index(S))
 
 
-    return curr
+def rev(tupl):
+    return (-tupl[0], -tupl[1])
+
+rows = len(mat)
+cols = len(mat[0])
+# start from S, navigate in BFS for
+# moves that are possible with pipes, legal and not visited (3 checks)
+
+OUT = {'.':[], "-": [(0, 1), (0, -1)], "|": [(1,0), (-1, 0)], "F": [(0, 1), (1, 0)], "J": [(-1, 0), (0, -1)], "7":[(1, 0), (0, -1)], "L": [(0, 1), (-1, 0)], 'S': [(0,1),(0,-1),(1,0),(-1,0)]}
+
+print(start)
+print(mat)
+from collections import deque
+pos = start
+q = deque()
+q.append((0, pos))
 
 
-with open("aoc9.txt", 'r') as f:
+mx = 0
+visited = set()
+
+while q:
+
+    steps, pos = q.popleft()
+    if pos in visited:
+        continue
+    visited.add(pos)
+    x,y = pos
+
+    mx = max(steps, mx)
+
+    symbol = mat[pos[0]][pos[1]]
+    for diff in OUT[symbol]:
+        dx, dy = diff
+        candx = x + dx
+        candy = y + dy
+        if candx<0 or candy<0 or candx>= rows or candy>=cols:
+            continue
+
+        symbol2 = mat[candx][candy]
+        if rev(diff) in OUT[symbol2]:
+            q.append((steps+1, (candx, candy)))
 
 
-    for line in f.readlines():
-        a = [int(x) for x in line.split()]
-        s+=process(a)
 
 
 
 
-
-print(s)
+print(mx)
 
 
