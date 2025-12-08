6da860f40a09d5b9623b0379dfc5d8bb2dde0823
carolinux <carolinegr@gmail.com>
2025-12-08 12:03:18 +0100

    pwn days 7 & 8

---
+++ pwn2025/7.py
@@ -0,0 +1,10 @@
+def main():
+    pw = "S4ntaCl4u5!" * 3
+    inp = input("Enter password: ")
+    if inp == pw:
+        print("parrotCTF is a great place to learn!")
+    else:
+        print("what is javert doing right now?")
+
+
+main()
