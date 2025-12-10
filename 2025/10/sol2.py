052b184fc2017dd45754569973583099bc11f740
carolinux <carolinegr@gmail.com>
2025-12-10 23:09:39 +0100

    wip

--- 2025/10/sol2.py
+++ 2025/10/sol2.py
@@ -4,6 +4,7 @@ import re
 from collections import defaultdict, Counter
 from operator import add, mul
 import heapq as hq
+from datetime import datetime as dt
 import functools as ft
 ans = 0
 
@@ -23,10 +24,10 @@ def apply(state, ix, togs, last, target, times):
         else:
             new_state.append(state[i])
         if new_state[i] > target[i]:
-            return None
+            return None, False
         if new_state[i] < target[i] and last[i]<=ix:
-            return None
-    return tuple(new_state)
+            return None, True
+    return tuple(new_state), True
 
 
 
@@ -40,7 +41,7 @@ def dpsolve(start, target, togs):
             last[num] = i
 
 
-    @ft.lru_cache(maxsize=None)
+    @ft.lru_cache(maxsize=15000000)
     def recur(i, curr):
         #print(f"{curr} vs {target}")
         if curr == target:
@@ -49,7 +50,9 @@ def dpsolve(start, target, togs):
             return math.inf
         minans = math.inf
         for j in range(iters):
-            new_state = apply(curr, i, togs, last, target, j)
+            new_state, cont = apply(curr, i, togs, last, target, j)
+            if cont is False:
+                break
             if new_state is None:
                 continue
             cand = recur(i+1, new_state) + j
@@ -59,70 +62,77 @@ def dpsolve(start, target, togs):
 
     return recur(0, start)
 
+maxx = 0
+maxb = 0
+with open("out.txt", "w") as w:
+    with open("small.txt", "r") as f:
+        for i, line in enumerate(f.readlines()):
+            #if i !=50:
+            #    continue
+            ix1 = line.index("]")
 
-with open("input.txt", "r") as f:
-    for i, line in enumerate(f.readlines()):
-        #if i !=50:
-        #    continue
-        ix1 = line.index("]")
+            ix2 = line.index("{")
+            toggles = line[ix1+2:ix2-1]
 
-        ix2 = line.index("{")
-        toggles = line[ix1+2:ix2-1]
+            target = line[ix2+1: len(line)-2]
 
-        target = line[ix2+1: len(line)-2]
+            target = eval(f"({target})")
+            toggles = toggles.split(" ")
+            togs = []
+            cnt = Counter()
+            for tog in toggles:
+                #print(toggles)
+                tup = eval(tog)
+                if isinstance(tup, int):
+                    tup = (tup,)
+                togs.append(tup)
+                for num in tup:
+                    cnt[num]+=1
 
-        target = eval(f"({target})")
-        toggles = toggles.split(" ")
-        togs = []
-        cnt = Counter()
-        for tog in toggles:
-            #print(toggles)
-            tup = eval(tog)
-            if isinstance(tup, int):
-                tup = (tup,)
-            togs.append(tup)
-            for num in tup:
-                cnt[num]+=1
+            #print(f"{len(target)}: {cnt}")
+            cnts = [(v, k) for k,v in cnt.items()]
+            cnts.sort(key=lambda x: (x[0], target[x[1]-1]))
+            maxx = max(max(target), maxx)
 
-        #print(f"{len(target)}: {cnt}")
-        cnts = [(v, k) for k,v in cnt.items()]
-        cnts.sort(key=lambda x: (x[0], target[x[1]-1]))
+            bytes_needed = len(target) * int(math.ceil(math.log2(max(target))))
+            maxb = max(bytes_needed, maxb)
+            #togs.sort(key=lambda x: len(x))
 
-        #togs.sort(key=lambda x: len(x))
+            togs2 = []
+            for c, v in cnts:
+                print(f"first putting the {c} that have value={v}")
+                for tog in togs:
+                    if v in tog and (tog not in togs2):
+                        togs2.append(tog)
+                        #print(togs2)
 
-        togs2 = []
-        for c, v in cnts:
-            print(f"first putting the {c} that have value={v}")
-            for tog in togs:
-                if v in tog and (tog not in togs2):
-                    togs2.append(tog)
-                    #print(togs2)
+            togs = togs2
+            #print(togs)
+            togs = [set(x) for x in togs]
+            print(f"{cnts[0][0]} out oof {len(target)} foor tc {i+1}, {target}, {togs}")
+            #continue
+            #sys.exit(0)
+            #continue
 
-        togs = togs2
-        #print(togs)
-        togs = [set(x) for x in togs]
-        print(f"{cnts[0][0]} out oof {len(target)} foor tc {i+1}, {target}, {togs}")
-        #continue
-        #sys.exit(0)
-        #continue
+            start = tuple([0] * len(target))
+            dist = {}
+            print(f"solving {i+1} - {target}")
 
-        start = tuple([0] * len(target))
-        dist = {}
-        print(f"solving {i+1} - {target}")
-        gcd = target[0]
-        for j in range(1, len(target)):
-            gcd = math.gcd(gcd, target[j])
+            #continue
+            #togs.sort(key=lambda x:len(x))t
+            time1 = dt.now()
+            best = dpsolve(start, target, togs)
+            time2 = dt.now()
+            print(best)
+            print(time2-time1)
+            w.write(f"{i+1},{best},{time2-time1}\n")
+            w.flush()
 
-        if gcd > 1:
-            print(f"={gcd} for i={i}")
-        #continue
-        #togs.sort(key=lambda x:len(x))
-        best = dpsolve(start, target, togs)
-        print(best)
-
-        ans+=best
-        #break
+            ans+=best
+            #break
 
 
 print(ans)
-
+#print(maxx)
+#print(maxb)
+# 17986 -- too low
