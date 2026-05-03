0c070f0435150a40e60be16325c9295301bd5a5f
carolinux <carolinegr@gmail.com>
2026-05-03 23:08:01 +0200

    2021: day 23

--- 2021/23/sol2.py
+++ 2021/23/sol2.py
@@ -2,17 +2,17 @@ import heapq as hq
 from copy import copy
 import sys
 
-cha = 'db'
-chb = 'cc'
-chc = 'ad'
-chd = 'ba'
+cha = 'dddb'
+chb = 'ccbc'
+chc = 'abad'
+chd = 'baca'
 
 # example
 
-#cha = 'ba'
-#chb = 'cd'
-#chc = 'bc'
-#chd = 'da'
+#cha = 'bdda'
+#chb = 'ccbd'
+#chc = 'bbac'
+#chd = 'daca'
 
 
 hallway = '.' * 11
@@ -22,7 +22,7 @@ targeti = {'a': 2, 'b': 4, 'c':6, 'd':8}
 cost = {'a': 1 , 'b':10, 'c':100, 'd':1000}
 
 # in chamb
-ixes = {'a': [0,1], 'b':[2,3], 'c':[4,5], 'd': [6,7]}
+ixes = {'a': [0,1,2,3], 'b':[4,5,6,7], 'c':[8,9,10,11], 'd': [12,13,14,15]}
 
 halls = (0,1,3,5,7,9,10)
 forbidden = (2,4,6,8)
@@ -45,8 +45,10 @@ class State:
         line1 = self.hallway
         #line1 = [ch if ch !='.' else '#' for ch in self.hallway ]
         #line1 = ''.join(line1)
-        line2 = ('#'*2) + self.chamb[0] + "#" + self.chamb[2] + "#" + self.chamb[4] + "#" + self.chamb[6] + ('#'*2)
-        line3 = ('#'*2) + self.chamb[1] + "#" + self.chamb[3] + "#" + self.chamb[5] + "#" + self.chamb[7] + ('#'*2)
+        line2 = ('#'*2) + self.chamb[0] + "#" + self.chamb[4] + "#" + self.chamb[8] + "#" + self.chamb[12] + ('#'*2)
+        line3 = ('#'*2) + self.chamb[1] + "#" + self.chamb[5] + "#" + self.chamb[9] + "#" + self.chamb[13] + ('#'*2)
+        line4 = ('#'*2) + self.chamb[2] + "#" + self.chamb[6] + "#" + self.chamb[10] + "#" + self.chamb[14] + ('#'*2)
+        line4 = ('#'*2) + self.chamb[3] + "#" + self.chamb[7] + "#" + self.chamb[11] + "#" + self.chamb[15] + ('#'*2)
         return "\n".join([11*"=", line1,line2,line3,11*"="])
 
     def __eq__(self, o):
@@ -58,12 +60,16 @@ class State:
 
 
     def new_entry(self, ch, curri): # new entry from the hallway
-        ix1, ix2 = ixes[ch]
+        ix1, ix2, ix3, ix4 = ixes[ch]
         ch1 = self.chamb[ix1]
         ch2 = self.chamb[ix2]
-        empty = ch1 == '.' and ch2 == '.'
-        halfempty = ch1 == '.' and ch2 == ch
-        if not (empty or halfempty):
+        ch3 = self.chamb[ix3]
+        ch4 = self.chamb[ix4]
+        empty4 = ch1 == '.' and ch2 == '.' and ch3 == '.' and ch4 == '.'
+        empty3 = ch1 == '.' and ch2 == '.' and ch3 == '.' and ch4 == ch
+        empty2 = ch1 == '.' and ch2 == '.' and ch3 == ch and ch4 == ch
+        empty1 = ch1 == '.' and ch2 == ch and ch3 == ch and ch4 == ch
+        if not (empty4 or empty3 or empty2 or empty1):
             return None
         origi = curri
         #assert i !=2
@@ -84,12 +90,18 @@ class State:
 
         new_hallway = list(self.hallway)
         new_chamb = list(self.chamb)
-        if halfempty:
+        if empty1:
             moves+=1
             new_chamb[ix1] = ch
-        else:
-            new_chamb[ix2] = ch
+        elif empty2:
             moves+=2
+            new_chamb[ix2] = ch
+        elif empty3:
+            moves+=3
+            new_chamb[ix3] = ch
+        else:
+            new_chamb[ix4] = ch
+            moves+=4
         score = cost[ch] * moves
         new_hallway[origi] = '.'
         new_state = State(str2(new_hallway), str2(new_chamb))
@@ -108,29 +120,54 @@ class State:
 
     def check(self, tch):
         cands = []
-        ix1, ix2 = ixes[tch]
+        ix1, ix2, ix3, ix4 = ixes[tch]
         ch1 = self.chamb[ix1]
         ch2 = self.chamb[ix2]
-        if ch1 == tch and ch2 == tch:
+        ch3 = self.chamb[ix3]
+        ch4 = self.chamb[ix4]
+        if ch1 == tch and ch2 == tch and ch3 ==tch and ch4 ==tch:
             return [] # settled
-        if ch1 == '.' and ch2 == '.':
+        if ch1 == '.' and ch2 == '.' and ch3 =='.' and ch4 == '.':
             return [] # no exit possible
-        if ch2 == tch and ch1 =='.':
+        if ch1 == '.' and ch2 == '.' and ch3 =='.' and ch4 == tch:
+            return [] # settled
+
+        if ch4 ==tch and ch3 ==tch and ch2 == tch and ch1 =='.':
             return []  # settled
+
+        if ch4 ==tch and ch3 ==tch and ch2 == '.' and ch1 =='.':
+            return []  # settled
+
+        if ch4 ==tch and ch3 =='.' and ch2 == '.' and ch1 =='.':
+            return []  # settled
+
 
         moves = 0
         new_chamb = list(self.chamb)
-        if ch1 == '.':
+        if ch1 != '.':
+            # move ch1
+            ch = ch1
+            moves+=1
+            new_chamb[ix1] = '.'
+        elif ch1 == '.' and ch2 != '.':
             # move ch2
             ch = ch2
             moves+=2
             new_chamb[ix2] = '.'
 
+        elif  ch1 == '.' and ch2 == '.' and ch3 != '.':
+            # move ch3
+            ch = ch3
+            moves+=3
+            new_chamb[ix3] = '.'
+        elif  ch1 == '.' and ch2 == '.' and ch3 == '.' and ch4 != '.':
+            # move ch4
+            ch = ch4
+            moves+=4
+            new_chamb[ix4] = '.'
         else:
-            # move ch1
-            ch = ch1
-            moves+=1
-            new_chamb[ix1] = '.'
+            raise Exception(f"should not have reached here {self.chamb}, tch={tch}")
+
 
         new_chamb = str2(new_chamb)
         curri = targeti[tch]
@@ -162,7 +199,7 @@ class State:
 
 
     def is_final(self):
-        return self.chamb == 'aabbccdd'
+        return self.chamb == 'aaaabbbbccccdddd'
 
 
 
